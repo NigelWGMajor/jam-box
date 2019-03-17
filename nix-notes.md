3e5e76327df64ebe88cde171e32a0b6ad6197c8f
Nigel Major <nigelwgmajor@gmail.com>
2019-03-16 18:10:06 -0600

    Initial setup using ng cli

---
+++ nix-notes.md
@@ -0,0 +1,22 @@
+# Jam-box
+
+## Initial setup
+
+```code
+ng new jam-box
+cd jam-box
+ng g c navigation
+ng generate c home
+ng g component schedule
+ng g component music
+ng g c studio
+ng g c projects
+ng g c contact
+ng g s storage
+ng g s authentication
+npm install
+npm start
+
+code .
+
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { MusicComponent } from './music/music.component';
+import { StudioComponent } from './studio/studio.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    ScheduleComponent,
+    MusicComponent,
+    StudioComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
