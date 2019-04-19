3df338eb01f33003b201d472cfe1f6a297bc0706
Nigel Major <nigelwgmajor@gmail.com>
2019-04-19 10:33:40 -0600

    Grid removed, flex remains.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,9 +10,9 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
    user: string= 'Visitor';
-   playerMessage: string = 'In order to make changes you must log in.  You are welcome to browse as a visitor.';;
+   playerMessage: string ;
   ngOnInit() {
-    this.playerMessage = 'In order to make changes you must log in.  You are welcome to browse as a visitor.';
+    this.playerMessage = 'You are not logged in as a player:  you may browse as a visitor!';
   }
 
 }
